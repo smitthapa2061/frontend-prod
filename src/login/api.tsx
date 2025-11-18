60c9efdadd9fd6bf62002e4d63b651d0ec8b019c
unknown <smitthapa808@gmail.com>
2025-11-18 18:22:32 +0545

    updated

--- src/login/api.tsx
+++ src/login/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-prod-bs4c.onrender.com/api",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3000/api",
   withCredentials: true,  // MUST BE HERE ONLY
 });
 
