fb5a3c427ad226f1062ae6e16c9b04ca5fdb5031
unknown <smitthapa808@gmail.com>
2025-11-24 23:37:51 +0545

    new update

--- src/login/api.tsx
+++ src/login/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-prod-bs4c.onrender.com/api",
+  baseURL: "http://localhost:3000/api",
   withCredentials: true,  // MUST BE HERE ONLY
 });
 
